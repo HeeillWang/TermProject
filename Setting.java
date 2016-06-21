226d96fe4a94dcf30d68d69be66dcb92db7b3e09
whitecloudy <alex9395@naver.com>
2016-06-21 18:40:33 +0900

    세팅 클래스로 변경
    인터페이스 -> 클래스
    내부 데이터는 스태틱으로 되어있으며 겟터 구현

--- Setting.java
+++ Setting.java
@@ -2,11 +2,25 @@ package com.example.heeill.termproject;
 
 enum font_size {S,M,L};
 
-public interface Setting {
+public class Setting {
     static font_size search_font = font_size.M;
     static font_size wordbook_font = font_size.M;
     static font_size quiz_font = font_size.M;
     static int quiz_repeat = 3;
 
-    public void setting_apply();
+    public static font_size getQuiz_font() {
+        return quiz_font;
+    }
+
+    public static int getQuiz_repeat() {
+        return quiz_repeat;
+    }
+
+    public static font_size getSearch_font() {
+        return search_font;
+    }
+
+    public static font_size getWordbook_font() {
+        return wordbook_font;
+    }
 }
